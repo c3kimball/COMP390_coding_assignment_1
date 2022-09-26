11c3902057465f0b45e8343afd0586486e83ab23
c3kimball <60229381+c3kimball@users.noreply.github.com>
2022-09-26 11:43:29 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Craig Kimball 9/26/22
+# COMP390 Assignment 1
+
+import meteor_data_class
+
+if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ meteor_data_class.py
@@ -0,0 +1,17 @@
+class MeteorDataEntry():
+    def __init__(meteor_name, id, nametype, recclass, mass, g, fall, year, reclat, reclong, GeoLocation, States, Counties):
+        meteor_name = meteor_name
+        id = id
+        nametype = nametype
+        recclass = recclass
+        mass = mass
+        g = g
+        fall = fall
+        year = year
+        reclat = reclat
+        reclong = reclong
+        GeoLocation = GeoLocation
+        States = States
+        Counties = Counties
+
+
