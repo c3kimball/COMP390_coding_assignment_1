4c2e4984303434e286c947d5c468f0acc07d394f
c3kimball <60229381+c3kimball@users.noreply.github.com>
2022-09-26 12:55:25 -0400

    Downloaded meteor_data.txt instead of just copy and pasting. Huge difference, now all 12 fields show up for every meteor.

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ import meteor_data_class
 if __name__ == '__main__':
     #  Reads in a data set of meteors found of Earth and prints a table of the ones that are over 2,900,000 grams and another table of the ones that fell after 2013
 
+    #  This will be a list of every meteor
+    all_meteors = []
     #  This will be the list of meteors that are over 2,000,000g and fell 2013 or later
     target_meteor_list = []
-    all_meteors = []
 
-    with open("meteor_data.txt") as f:
+    with open("meteor_data.txt", "r") as f:
         while f.readline() != "":
             line = f.readline()
-            line1 = line.strip()
+            line1 = line.replace("\n", "")
             line2 = line1.split('\t')
+
+
             print(line2)
-
-
-
-
-
