04e9c4ea924b6db416d99a54706bdcb474263827
c3kimball <60229381+c3kimball@users.noreply.github.com>
2022-09-26 12:26:38 -0400

    I can read in the meteor_data.txt line by line without errors, but it is not printing the entire file. I am unsure if this is because the console is running out of space or if there is something very wrong with the code.

--- main.py
+++ main.py
@@ -4,6 +4,20 @@
 import meteor_data_class
 
 if __name__ == '__main__':
+    #  Reads in a data set of meteors found of Earth and prints a table of the ones that are over 2,900,000 grams and another table of the ones that fell after 2013
+
+    #  This will be the list of meteors that are over 2,000,000g and fell 2013 or later
+    target_meteor_list = []
+    all_meteors = []
+
+    with open("meteor_data.txt") as f:
+        while f.readline() != "":
+            line = f.readline()
+            line1 = line.strip()
+            line2 = line1.split('\t')
+            print(line2)
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
